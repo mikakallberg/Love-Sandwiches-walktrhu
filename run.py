9f71faa2b9866c19b7f92368a62f0c3f166f8770
mikakallberg <mikaela.kallberg@hotmail.com>
2022-05-03 17:28:14 +0000

    Change commented out code

--- run.py
+++ run.py
@@ -58,17 +58,16 @@ def validate_data(values):
     
     return True
 
-
+"""
 def update_sales_worksheet(data):
-    """
+
     Update sales worksheet, add new row with the list data provided
-    """
+    
     print('Updating sales worksheet...\n')
     sales_worksheet = SHEET.worksheet('sales')
     sales_worksheet.append_row(data)
     print('Sales worksheet updated successfully!\n')
 
-"""
 def update_surplus_worksheet(data):
     
     Update surplus worksheet, add new row with the list data provided
@@ -77,17 +76,18 @@ def update_surplus_worksheet(data):
     surplus_worksheet = SHEET.worksheet('surplus')
     surplus_worksheet.append_row(data)
     print('Surplus worksheet updated successfully!\n')
+"""
 
 def update_worksheet(data, worksheet):
-    
+    """
     Recieve a list of integers to be inserted into a worksheet
     Update the relevant worksheet with data provided
-    
+    """
     print(f'Updating {worksheet} worksheet...\n')
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
     print(f'{worksheet} worksheet updated successfully!\n')
-"""
+
 
 def calculate_surplus_data(sales_row):
     """
